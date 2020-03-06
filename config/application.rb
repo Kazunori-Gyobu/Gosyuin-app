7ccddae4026bf2a91271646a352c90a0670b80ac
Kazunori Gyobu <kgyoby@gmail.com>
2020-03-06 21:27:07 +0900

    Homeコントローラーを追加

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module GApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root,join('config', 'locals', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locals', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.test_framework :rspec,
