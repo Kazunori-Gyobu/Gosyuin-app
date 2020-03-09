b3498a4bf137bc328117075115d349d76017ba74
Kazunori Gyobu <kgyoby@gmail.com>
2020-03-09 23:13:33 +0900

    ユーザー登録ページを設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
+  devise_for :users, skip: [:sessions]
   devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-    post 'login', to: 'devise/sessions#create'
-    delete 'signout', to: 'devise/sessions#destroy'
+    get 'login', to: 'devise/sessions#new', as: :new_user_session
+    post 'login', to: 'devise/sessions#create', as: :user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 end
