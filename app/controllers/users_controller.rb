1d06a3a63405edf0fae79f4e52ab916c0e8d4ba0
Kazunori Gyobu <kgyoby@gmail.com>
2020-03-18 19:35:39 +0900

    gemKaminariをupdate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(15)
   end
 
   def show
