cdb214046a19994818fd5af66bfa1bf01b00092f
Kazunori Gyobu <kgyoby@gmail.com>
2020-03-21 16:33:13 +0900

    フォロー機能のテスト完了

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :relationship do
-    association :follower, factory: :user
-    association :followed, factory: :user, email: 'another@test.com'
+    
   end
 end
--- spec/system/follow_spec.rb
+++ spec/system/follow_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Follow", type: :system, js: true do
+RSpec.describe "Follow", type: :system do
   let!(:user) { FactoryBot.create(:user, name: "斎藤") }
   let!(:another_user) { FactoryBot.create(:user, name: "山本") }
   let!(:active) { user.active_relationships.create(followed_id: another_user.id) }
@@ -11,7 +11,7 @@ RSpec.describe "Follow", type: :system, js: true do
 
   describe "フォロー/フォロー解除のボタン" do
 
-    it "フォローしているユーザーの名前があること" do
+    it "フォロー解除ボタンが有効なこと" do
       visit user_path(user)
       click_link "following"
       expect(current_path).to eq following_user_path(user)
@@ -21,9 +21,12 @@ RSpec.describe "Follow", type: :system, js: true do
       expect(current_path).to eq user_path(another_user)
       expect(page).to have_selector ".edit_relationship"
       expect {
-        click_button "フォロー解除"
+      click_on "フォロー解除"
       }.to change { user.following.count }.by(-1)
+    end
 
+    it "フォローボタンが有効なこと" do
+      
     end
   end
 end
