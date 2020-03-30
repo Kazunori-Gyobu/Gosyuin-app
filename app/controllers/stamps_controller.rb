fb31c8d385938f648cd52cd3131b59cc68ddc1ca
Kazunori Gyobu <kgyoby@gmail.com>
2020-03-30 11:29:18 +0900

    御朱印一覧のCSS調整

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,4 +1,5 @@
 class StampsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_select_collections, only: [:new, :create, :edit, :update]
 
   def index
@@ -25,22 +26,34 @@ class StampsController < ApplicationController
   end
 
   def edit
-    @stampbooks = Stampbook.all
     @stamp = Stamp.find(params[:id])
   end
 
   def update
-    
+    @stamp = current_user.stamps.find_by(id: params[:id])
+    if @stamp.update_attributes(stamp_params)
+      flash[:notice] = "御朱印の編集に成功しました"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    @stamp.destroy
+    flash[:notice] = "御朱印を削除しました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def stamp_params
     params.require(:stamp).permit(:name, :given_date, :photo, :remove_photo, :remarks, :distinction, :stampbook_id)
+  end
+
+  def correct_user
+    @stamp = current_user.stamps.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 
   def set_select_collections
